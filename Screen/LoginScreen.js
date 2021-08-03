d7cf5f392e67097d5f669d6b5dde1f7232132f1a
sumit245 <sumitranjan245@gmail.com>
2021-08-03 09:06:42 +0530

    Update

--- Screen/LoginScreen.js
+++ Screen/LoginScreen.js
@@ -2,236 +2,240 @@
 // https://aboutreact.com/react-native-login-and-signup/
 
 // Import React and Component
-import React, { useState, createRef } from 'react';
+import React, {useState, createRef} from 'react';
 import {
-    StyleSheet,
-    TextInput,
-    View,
-    Text,
-    ScrollView,
-    Image,
-    Keyboard,
-    TouchableOpacity,
-    KeyboardAvoidingView,
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
 } from 'react-native';
 
 import AsyncStorage from '@react-native-community/async-storage';
 
 import Loader from './Components/Loader';
-import { Checkbox } from 'react-native-paper';
+import {Checkbox} from 'react-native-paper';
 
-const LoginScreen = ({ navigation }) => {
-    const [userEmail, setUserEmail] = useState('');
-    const [userPassword, setUserPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [errortext, setErrortext] = useState('');
+const LoginScreen = ({navigation}) => {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errortext, setErrortext] = useState('');
 
-    const passwordInputRef = createRef();
+  const passwordInputRef = createRef();
 
-    const handleSubmitPress = () => {
-        setErrortext('');
-        if (!userEmail) {
-            alert('Please fill Email');
-            return;
+  const handleSubmitPress = () => {
+    setErrortext('');
+    if (!userEmail) {
+      alert('Please fill Email');
+      return;
+    }
+    if (!userPassword) {
+      alert('Please fill Password');
+      return;
+    }
+    setLoading(true);
+    let dataToSend = {email: userEmail, password: userPassword};
+    // let formBody = [];
+    // for (let key in dataToSend) {
+    //   let encodedKey = encodeURIComponent(key);
+    //   let encodedValue = encodeURIComponent(dataToSend[key]);
+    //   formBody.push(encodedKey + '=' + encodedValue);
+    // }
+    // formBody = formBody.join('&');
+    fetch('http://192.168.1.4:5000/api/users/login', {
+      method: 'POST',
+      body: dataToSend,
+      headers: {
+        //Header Defination
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        //Hide Loader
+        setLoading(false);
+        console.log(responseJson);
+        // If server response message same as Data Matched
+        if (responseJson.status === 'success') {
+          AsyncStorage.setItem('user_id', responseJson.data.email);
+          console.log(responseJson.data.email);
+          navigation.replace('DrawerNavigationRoutes');
+        } else {
+          setErrortext(responseJson.msg);
+          console.log('Please check your email id or password');
         }
-        if (!userPassword) {
-            alert('Please fill Password');
-            return;
-        }
-        setLoading(true);
-        let dataToSend = { email: userEmail, password: userPassword };
-        let formBody = [];
-        for (let key in dataToSend) {
-            let encodedKey = encodeURIComponent(key);
-            let encodedValue = encodeURIComponent(dataToSend[key]);
-            formBody.push(encodedKey + '=' + encodedValue);
-        }
-        formBody = formBody.join('&');
-        fetch('http://192.168.1.4:3000/api/user/login', {
-            method: 'POST',
-            body: formBody,
-            headers: {
-                //Header Defination
-                'Content-Type':
-                    'application/x-www-form-urlencoded;charset=UTF-8',
-            },
-        })
-            .then((response) => response.json())
-            .then((responseJson) => {
-                //Hide Loader
-                setLoading(false);
-                console.log(responseJson);
-                // If server response message same as Data Matched
-                if (responseJson.status === 'success') {
-                    AsyncStorage.setItem('user_id', responseJson.data.email);
-                    console.log(responseJson.data.email);
-                    navigation.replace('DrawerNavigationRoutes');
-                } else {
-                    setErrortext(responseJson.msg);
-                    console.log('Please check your email id or password');
+      })
+      .catch(error => {
+        //Hide Loader
+        setLoading(false);
+        console.error(error);
+      });
+  };
+  const [checked, setChecked] = React.useState(false);
+  return (
+    <View style={styles.mainBody}>
+      <Loader loading={loading} />
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}>
+        <View>
+          <KeyboardAvoidingView enabled>
+            <View style={{alignItems: 'center'}}>
+              <Image
+                source={require('../Image/aboutreact.png')}
+                style={{
+                  width: '50%',
+                  height: 100,
+                  resizeMode: 'contain',
+                  margin: 30,
+                }}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={UserEmail => setUserEmail(UserEmail)}
+                placeholder="Enter Email" //dummy@abc.com
+                placeholderTextColor="#fff"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                returnKeyType="next"
+                onSubmitEditing={() =>
+                  passwordInputRef.current && passwordInputRef.current.focus()
                 }
-            })
-            .catch((error) => {
-                //Hide Loader
-                setLoading(false);
-                console.error(error);
-            });
-    };
-    const [checked, setChecked] = React.useState(false);
-    return (
-        <View style={styles.mainBody}>
-            <Loader loading={loading} />
-            <ScrollView
-                keyboardShouldPersistTaps="handled"
-                contentContainerStyle={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignContent: 'center',
-                }}>
-                <View>
-                    <KeyboardAvoidingView enabled>
-                        <View style={{ alignItems: 'center' }}>
-                            <Image
-                                source={require('../Image/aboutreact.png')}
-                                style={{
-                                    width: '50%',
-                                    height: 100,
-                                    resizeMode: 'contain',
-                                    margin: 30,
-                                }}
-                            />
-                        </View>
-                        <View style={styles.SectionStyle}>
-                            <TextInput
-                                style={styles.inputStyle}
-                                onChangeText={(UserEmail) =>
-                                    setUserEmail(UserEmail)
-                                }
-                                placeholder="Enter Email" //dummy@abc.com
-                                placeholderTextColor="#fff"
-                                autoCapitalize="none"
-                                keyboardType="email-address"
-                                returnKeyType="next"
-                                onSubmitEditing={() =>
-                                    passwordInputRef.current &&
-                                    passwordInputRef.current.focus()
-                                }
-                                underlineColorAndroid="#f000"
-                                blurOnSubmit={false}
-                            />
-                        </View>
-                        <View style={styles.SectionStyle}>
-                            <TextInput
-                                style={styles.inputStyle}
-                                onChangeText={(UserPassword) =>
-                                    setUserPassword(UserPassword)
-                                }
-                                placeholder="Enter Password" //12345
-                                placeholderTextColor="#fff"
-                                keyboardType="default"
-                                ref={passwordInputRef}
-                                onSubmitEditing={Keyboard.dismiss}
-                                blurOnSubmit={false}
-                                secureTextEntry={!checked}
-                                underlineColorAndroid="#f000"
-                                returnKeyType="next"
-                            />
-                        </View>
-                        {errortext != '' ? (
-                            <Text style={styles.errorTextStyle}>
-                                {errortext}
-                            </Text>
-
-                        ) : null}
-                        <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 28 }}>
-                            <Checkbox
-                                uncheckedColor="#FFFFFF"
-                                color="#7DE24E"
-                                status={checked ? 'checked' : 'unchecked'}
-                                onPress={() => {
-                                    setChecked(!checked);
-                                }}
-                            />
-                            <Text style={{ color: "#fff", fontWeight: 'bold', fontSize: 14 }}>Show Password</Text>
-                        </View>
-                        <Text style={styles.errorTextStyle}>
-                            Forgot Password?
-                        </Text>
-                        <TouchableOpacity
-                            style={styles.buttonStyle}
-                            activeOpacity={0.5}
-                            onPress={handleSubmitPress}>
-                            <Text style={styles.buttonTextStyle}>LOGIN</Text>
-                        </TouchableOpacity>
-                        <Text
-                            style={styles.registerTextStyle}
-                            onPress={() => navigation.navigate('RegisterScreen')}>
-                            New Here ? Register
-                        </Text>
-                        <Text style={{ color: "#FFF", fontWeight: 'bold', fontSize: 14, padding: 4, marginLeft: 6, textAlign: "center" }}>
-                            Need Help?
-                        </Text>
-                    </KeyboardAvoidingView>
-                </View>
-            </ScrollView>
+                underlineColorAndroid="#f000"
+                blurOnSubmit={false}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={UserPassword => setUserPassword(UserPassword)}
+                placeholder="Enter Password" //12345
+                placeholderTextColor="#fff"
+                keyboardType="default"
+                ref={passwordInputRef}
+                onSubmitEditing={Keyboard.dismiss}
+                blurOnSubmit={false}
+                secureTextEntry={!checked}
+                underlineColorAndroid="#f000"
+                returnKeyType="next"
+              />
+            </View>
+            {errortext != '' ? (
+              <Text style={styles.errorTextStyle}>{errortext}</Text>
+            ) : null}
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginLeft: 28,
+              }}>
+              <Checkbox
+                uncheckedColor="#FFFFFF"
+                color="#7DE24E"
+                status={checked ? 'checked' : 'unchecked'}
+                onPress={() => {
+                  setChecked(!checked);
+                }}
+              />
+              <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 14}}>
+                Show Password
+              </Text>
+            </View>
+            <Text style={styles.errorTextStyle}>Forgot Password?</Text>
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              activeOpacity={0.5}
+              onPress={handleSubmitPress}>
+              <Text style={styles.buttonTextStyle}>LOGIN</Text>
+            </TouchableOpacity>
+            <Text
+              style={styles.registerTextStyle}
+              onPress={() => navigation.navigate('RegisterScreen')}>
+              New Here ? Register
+            </Text>
+            <Text
+              style={{
+                color: '#FFF',
+                fontWeight: 'bold',
+                fontSize: 14,
+                padding: 4,
+                marginLeft: 6,
+                textAlign: 'center',
+              }}>
+              Need Help?
+            </Text>
+          </KeyboardAvoidingView>
         </View>
-    );
+      </ScrollView>
+    </View>
+  );
 };
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-    mainBody: {
-        flex: 1,
-        justifyContent: 'center',
-        backgroundColor: '#307ecc',
-        alignContent: 'center',
-    },
-    SectionStyle: {
-        flexDirection: 'row',
-        height: 40,
-        marginTop: 20,
-        marginLeft: 35,
-        marginRight: 35,
-        margin: 10,
-    },
-    buttonStyle: {
-        backgroundColor: '#7DE24E',
-        borderWidth: 0,
-        color: '#FFFFFF',
-        borderColor: '#7DE24E',
-        height: 40,
-        alignItems: 'center',
-        borderRadius: 6,
-        marginLeft: 35,
-        marginRight: 35,
-        marginTop: 20,
-        marginBottom: 25,
-    },
-    buttonTextStyle: {
-        color: '#FFFFFF',
-        paddingVertical: 10,
-        fontSize: 16,
-    },
-    inputStyle: {
-        flex: 1,
-        color: 'white',
-        paddingLeft: 15,
-        paddingRight: 15,
-        borderWidth: 1,
-        borderRadius: 6,
-        borderColor: '#dadae8',
-    },
-    registerTextStyle: {
-        color: '#FFFFFF',
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontSize: 14,
-        alignSelf: 'center',
-        padding: 10,
-    },
-    errorTextStyle: {
-        color: 'red',
-        textAlign: 'center',
-        fontSize: 14,
-    },
+  mainBody: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#307ecc',
+    alignContent: 'center',
+  },
+  SectionStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: '#7DE24E',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 6,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
+  },
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  inputStyle: {
+    flex: 1,
+    color: 'white',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 6,
+    borderColor: '#dadae8',
+  },
+  registerTextStyle: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+    alignSelf: 'center',
+    padding: 10,
+  },
+  errorTextStyle: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 14,
+  },
 });
